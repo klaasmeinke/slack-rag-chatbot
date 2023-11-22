e9088963f75b9f6b4fd6b21a3beee346738032ba
Tibi <tiberiu@hadrian.io>
2023-11-22 11:47:08 +0100

    remove by source if already exists

--- hadriangpt/bot/answer.py
+++ hadriangpt/bot/answer.py
@@ -7,20 +7,21 @@ from hadriangpt.bot.vectorstore import VectorDB
 class Bot:
     def __init__(self, openai_model: str = "gpt-4-1106-preview"):
         llm = ChatOpenAI(model_name=openai_model, temperature=0.1)
-        self.vectorstore = VectorDB()
+        self._vectorstore = VectorDB()
         self.chain = RetrievalQAWithSourcesChain.from_chain_type(
-            llm=llm, retriever=self.vectorstore.as_retriever()
+            llm=llm, retriever=self._vectorstore.as_retriever()
         )
 
     def run(self, question: str):
         return self.chain({"question": question})
 
     def add_docs(self, docs):
-        self.vectorstore.add_docs(docs)
+        self._vectorstore.add_docs(docs)
 
 
 if __name__ == "__main__":
-    doc = Document(page_content="The meaning of life is 42", metadata={"source": "42-c"})
     bot = Bot()
-    bot.add_docs([doc])
+    bot.add_docs([Document(page_content="The meaning of life is 42", metadata={"source": "42-c"})])
+    print(bot.run("What is the meaning of life?"))
+    bot.add_docs([Document(page_content="The meaning of life is 69", metadata={"source": "42-c"})])
     print(bot.run("What is the meaning of life?"))
--- hadriangpt/bot/vectorstore.py
+++ hadriangpt/bot/vectorstore.py
@@ -10,11 +10,28 @@ class VectorDB:
         embeddings_model = OpenAIEmbeddings()
         embedding_size = 1536
         index = faiss.IndexFlatL2(embedding_size)
-        self.vectorstore_public = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
-        self.max_tokens_per_doc = 500
+        self._vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+        self._max_tokens_per_doc = 500
 
     def add_docs(self, docs):
-        self.vectorstore_public.add_documents(docs)
+        # todo: split
+        sources = list(set([doc.metadata['source'] for doc in docs]))
+        for source in sources:
+            self.delete_by_source(source)
+        self._vectorstore.add_documents(docs)
+
+    def delete_by_source(self, source):
+        # look up docs by source
+        d = self._vectorstore.docstore._dict
+        to_delete = []
+        for key in d:
+            if d[key].metadata['source'] == source:
+                to_delete.append(key)
+
+        if not to_delete:
+            return
+
+        self._vectorstore.delete(to_delete)
 
     def as_retriever(self):
-        return self.vectorstore_public.as_retriever()
+        return self._vectorstore.as_retriever()
