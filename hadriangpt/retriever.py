2c254185c3a7dfe08e3db5cbb22cd369dde18c20
klaas <48212135+klaasmeinke@users.noreply.github.com>
2023-12-11 11:00:45 +0100

    samll simplification

--- hadriangpt/retriever.py
+++ hadriangpt/retriever.py
@@ -33,9 +33,8 @@ class Doc(BaseModel):
 class Retriever:
     def __init__(self):
         self.openai_client = OpenAI()
-        self.docs: Dict[str, Doc] = dict()
-        self.embeddings_cache: Dict[str, List[float]] = dict()
-        self.load_embedding_file()
+        self.docs: List[Doc] = []
+        self.embeddings_cache: Dict[str, List[float]] = dict()  # key = content hash, value = content embedding
         self.add_notion_pages()
 
     def __call__(self, query: str) -> List[str]:
@@ -43,20 +42,19 @@ class Retriever:
         query_vector = np.asarray(query_embedding)
         query_vector_norm = query_embedding / np.linalg.norm(query_vector)
 
-        embeddings_matrix = np.asarray(list(self.embeddings_cache.values()))
+        embeddings_matrix = np.asarray([doc.embedding for doc in self.docs])
         embeddings_matrix_norm = embeddings_matrix / np.linalg.norm(embeddings_matrix, axis=1, keepdims=True)
         similarities = embeddings_matrix_norm.dot(query_vector_norm)
 
-        token_count, selected_docs = 0, []
-        for index in np.argsort(similarities)[::-1]:
-            doc_id = list(self.embeddings_cache.keys())[index]
-            doc = self.docs[doc_id]
+        token_count, selected_strings = 0, []
+        for i in np.argsort(similarities)[::-1]:
+            doc = self.docs[i]
             token_count += doc.token_count
             if token_count > Config.query_token_limit:
                 break
-            selected_docs.append(str(doc))
+            selected_strings.append(str(doc))
 
-        return selected_docs
+        return selected_strings
 
     def add_notion_pages(self):
         notion = Notion()
@@ -64,34 +62,35 @@ class Retriever:
             if not str(page).strip():
                 continue
             doc = Doc(content=str(page), source=page.url)
-            self.docs[doc.content_hash] = doc
+            self.docs.append(doc)
         self.add_embeddings_to_docs()
 
     def add_embeddings_to_docs(self):
         # add embeddings to cache
-        docs_without_embeddings = {key: doc for key, doc in self.docs.items() if key not in self.embeddings_cache}
+        self.load_embeddings_cache()
+        docs_without_embeddings = [doc for doc in self.docs if doc.content_hash not in self.embeddings_cache]
         if docs_without_embeddings:
             print('fetching embeddings...')
-            for key, doc in tqdm(docs_without_embeddings.items()):
-                if key in self.embeddings_cache:
+            for doc in tqdm(docs_without_embeddings):
+                if doc.content_hash in self.embeddings_cache:
                     continue
-                self.embeddings_cache[key] = self.fetch_embedding(doc.content)
-                self.save_embeddings_file()
+                self.embeddings_cache[doc.content_hash] = self.fetch_embedding(doc.content)
+                self.save_embeddings_cache()
 
         # add embeddings to docs
-        for key, doc in self.docs.items():
-            doc.embedding = self.embeddings_cache[key]
+        for doc in self.docs:
+            doc.embedding = self.embeddings_cache[doc.content_hash]
 
     def fetch_embedding(self, text: str):
         text = text.replace("\n", " ")
         return self.openai_client.embeddings.create(input=[text], model=Config.embeddings_model).data[0].embedding
 
-    def save_embeddings_file(self):
+    def save_embeddings_cache(self):
         os.makedirs(Config.data_dir, exist_ok=True)
         with open(Config.embeddings_cache_file, 'w') as f:
             json.dump(self.embeddings_cache, f)
 
-    def load_embedding_file(self):
+    def load_embeddings_cache(self):
         if os.path.exists(Config.embeddings_cache_file):
             with open(Config.embeddings_cache_file) as json_file:
                 self.embeddings_cache = json.load(json_file)
